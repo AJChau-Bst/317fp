b61873b49ab9fa52127b23d6bfc6c6031cf453e3
marleigha <118072178+marleigha@users.noreply.github.com>
2024-01-01 14:08:19 -0500

    int. commit
    grabbing things from firestore successfully and working to build out displaying your friends

--- 317fp/components/FriendsScreen.js
+++ 317fp/components/FriendsScreen.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext, Component } from 'react';
 import { Button, Text, View, TextInput, StyleSheet, Image, TouchableOpacity, SafeAreaView } from 'react-native';
 import StateContext from './StateContext.js';
 import { getDoc, arrayUnion, setDoc, doc } from "firebase/firestore";
@@ -7,38 +7,37 @@ import styles from "./styles.js";
 
 export default function FriendsScreen() {
 
-    const [friendInputText, setFriendInputText] = useState('');
-    const [isRequestingFriend, setIsRequestingFriend] = useState(false);
-    const [friendsList, setFriendsList] = useState([])
+  const [friendInputText, setFriendInputText] = useState('');
+  const [isRequestingFriend, setIsRequestingFriend] = useState(false);
+  const [friendsList, setFriendsList] = useState([])
 
 
-    const {firebaseProps, socialProps} = useContext(StateContext);
-    console.log("here is socialProps: ", socialProps);
-    const db = firebaseProps.db;
-    const auth = firebaseProps.auth;
-    const email = auth.currentUser?.email
+  const { firebaseProps, socialProps } = useContext(StateContext);
+  console.log("here is socialProps: ", socialProps);
+  const db = firebaseProps.db;
+  const auth = firebaseProps.auth;
+  const email = auth.currentUser?.email
 
-    /*async function requestFriend(friendEmail) {
-      const now = new Date();
-      const curTimestamp = now.getTime()
-      await setDoc(doc(db, "FriendRequests", curTimestamp),
-        {
-          requestTo: friendEmail,
-          requestFrom: email,
-          timestamp: curTimestamp,
+  /*async function requestFriend(friendEmail) {
+    const now = new Date();
+    const curTimestamp = now.getTime()
+    await setDoc(doc(db, "FriendRequests", curTimestamp),
+      {
+        requestTo: friendEmail,
+        requestFrom: email,
+        timestamp: curTimestamp,
 
-        });
-    }*/
+      });
+  }*/
 
-     function fetchFriends() {
-      //console.log("email in fetchFriends: ", email)
-      const docRef = doc(db, "FriendsLists", email);
-      console.log("This is the docRef: ",docRef);
-      getDoc(docRef).then(
-        (docSnap) => {
+  function fetchFriends() {
+    //console.log("email in fetchFriends: ", email)
+    const docRef = doc(db, "FriendsLists", email);
+    console.log("This is the docRef: ", docRef);
+    getDoc(docRef).then(
+      (docSnap) => {
         if (docSnap.exists()) {
-            console.log("This is the docSnap: ", docSnap.data());
-          console.log("Document data from friendArray:", docSnap.data().friendArray);
+          //console.log("This is the docSnap: ", docSnap.data());
           setFriendsList(prevFriendList => (docSnap.data().friendArray));
           //console.log(friendsList);
         } else {
@@ -46,27 +45,35 @@ export default function FriendsScreen() {
           console.log("No such document!");
         }
       });
-      
-    }
-
-    fetchFriends();
-
-    //we'll probably sort it into two different sections
-    // the top will give you the option to add a friend
-    return (
-      <View>
-        <Text>Friends Screen!</Text>
-        <TextInput
-          style={styles.friendInput}
-          onChangeText={setFriendInputText}
-          value={friendInputText}
-          placeholder="sample@email.com"
-    />
-        <Text>{email}'s friends</Text>
-        <Text>{JSON.stringify(friendsList)}</Text>
-
-      </View>
-    
-    )
 
   }
+  fetchFriends();
+  /*
+  // need to do like a whole components thing? maybe make it a touchable opacity? who knows
+  function formatFriends(friendArr) {
+    friendArr.forEach(element => {
+      return (
+        <ul>{JSON.stringify(element)}</ul>
+      )
+    });
+  }*/
+
+  //we'll probably sort it into two different sections
+  // the top will give you the option to add a friend
+  return (
+    <View>
+      <Text>Friends Screen!</Text>
+      <TextInput
+        style={styles.friendInput}
+        onChangeText={setFriendInputText}
+        value={friendInputText}
+        placeholder="sample@email.com"
+      />
+      <Text>{email}'s friends</Text>
+      <Text>{JSON.stringify(friendsList)}</Text>
+
+    </View>
+
+  )
+
+}
